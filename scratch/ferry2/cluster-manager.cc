d0be2897145d9f7a84e4c837e6661652f347e600
spyit1 <sota22531.pro@gmail.com>
2026-08-03 02:29:07 +0900

    first commit 20260803

---
+++ scratch/ferry2/cluster-manager.cc
@@ -0,0 +1,5 @@
+#include "cluster-manager.h"
+
+ClusterManager::ClusterManager()
+{
+}
---
+++ scratch/ferry2/cluster-manager.h
@@ -0,0 +1,10 @@
+#ifndef CLUSTER_MANAGER_H
+#define CLUSTER_MANAGER_H
+
+class ClusterManager
+{
+public:
+    ClusterManager();
+};
+
+#endif // CLUSTER_MANAGER_H
